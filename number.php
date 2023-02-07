d7c9f654a496d3759aeb09ae4ddfba468347e648
al-jabir <aljabir098@gmail.com>
2023-02-07 23:25:09 +0600

    number

---
+++ number.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number of php</title>
+</head>
+
+<body>
+    <?php
+    $x = 'king';
+    $a = 20;
+    $s = 19.98;
+    $t = true;
+    echo var_dump(is_string($x));
+    echo "<br/>";
+    echo var_dump(is_string($a));
+    echo "<br/>";
+    echo var_dump(is_numeric($a));
+    echo "<br/>";
+    echo var_dump(is_int($a));
+    echo "<br/>";
+    echo var_dump(is_int($s));
+    echo "<br/>";
+    echo var_dump(is_float($a));
+    echo "<br/>";
+    echo var_dump(is_float($s));
+    echo "<br/>";
+    echo var_dump(is_bool($t));
+
+    echo "<br/>";
+    $s_cast = (int)$s;
+    echo $s_cast;
+    ?>
+</body>
+
+</html>
