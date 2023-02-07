c9ddd55481990d8e1af3691fef65cbbc671b6d73
al-jabir <aljabir098@gmail.com>
2023-02-07 23:15:16 +0600

    string

---
+++ string.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String of php</title>
+</head>
+
+<body>
+    <?php
+    // string length
+
+    $a = "How to I beacame a programmer";
+    echo strlen($a);
+    echo strrev($a);
+    echo strpos("Frontend developer is best", "is");
+    echo "<br/>";
+
+    echo str_replace("Frontend", "developer", "Backend developer is best")
+
+    ?>
+</body>
+
+</html>
