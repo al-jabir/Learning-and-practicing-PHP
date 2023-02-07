01e059ef9ccb0fb8dbc50d9c8dc73dd6431a3102
al-jabir <aljabir098@gmail.com>
2023-02-08 00:23:03 +0600

    thats awesome php

--- test.php
+++ test.php
@@ -4,4 +4,23 @@ function myCode($a){
 }
 
 echo myCode('php programming');
+
+echo "<br/>";
+
+class Person {
+    public $name;
+    public $age;
+
+    public function __construct($name, $age){
+        $this -> name = $name;
+        $this -> age = $age;
+    }
+
+    public function getDetails(){
+        return "My name is " . $this -> name . " and I am " . $this -> age . " Years old.";
+    }
+}
+
+$getResult = new Person("King",25);
+echo $getResult -> getDetails();
 ?>
