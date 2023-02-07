a877a5c578c5c5bc82180fbfff0e671d20b1edb6
al-jabir <aljabir098@gmail.com>
2023-02-07 22:15:44 +0600

    variable

---
+++ variable.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable of php</title>
+</head>
+
+<body>
+    <?php
+    // variables
+    $name = "Jabir";
+    echo "My name is  $name <br/>";
+
+    $age = 25;
+    echo "I am  $age years old";
+
+    // string contcat
+
+    echo "<h3>I am $name. I am $age years old.</h3>"
+    ?>
+</body>
+
+</html>
