059004789b7d914e3fb84d26489ed819a3025289
al-jabir <aljabir098@gmail.com>
2023-02-08 10:52:11 +0600

    test and condition

---
+++ condition.php
@@ -0,0 +1,12 @@
+<?php 
+echo "<h1>learning condition of php <br/></h1>";
+
+$x = 'mafia';
+if($x == 'mafias'){
+    echo "<strong>I will be learning it</strong><br/>";
+}
+else{
+    echo "<strong>I will be good learning of php</strong>";
+}
+
+?>
