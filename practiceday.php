b9b9bc0f89386093d429b6a2ae6d156ee13b772e
al-jabir <aljabir098@gmail.com>
2023-02-09 21:16:54 +0600

    object

--- practiceday.php
+++ practiceday.php
@@ -23,4 +23,18 @@ echo  "My name is $my_name" . '<br />' ;
 echo  "My name is $My_Nick" . '<br />' ;
 echo  "I am $age_7 years old" . '<br />' ;
 
+// Object
+
+class Phone{
+    var $brand;
+    function getModel($num){
+        global $brand;
+        $brand = $num;
+        echo 'This is brand name ' . $brand . '.';
+    }
+}
+$getResult = new Phone;
+
+$getResult -> getModel('OnePlus 9 pro');
+
 ?>
