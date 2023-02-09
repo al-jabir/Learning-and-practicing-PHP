0297acdfb1b49c924600fc1be0de32be155091de
al-jabir <aljabir098@gmail.com>
2023-02-09 21:07:08 +0600

    variable done

--- practiceday.php
+++ practiceday.php
@@ -5,16 +5,22 @@ echo "<h3>My name is $name</h3>";
 
 echo "<br/>";
 
-echo 'Hey are you ' .$name;
+echo 'Hey are you ' .$name; 
+
+echo '<br/>';
 
 // variable name rules
-/*
+
 $name = 'King';
 
 $my_name = 'John';
 
 $My_Nick = 'Mafia';
 $age_7 = '7';
-*/
+
+echo  "My name is $name" . '<br />' ;
+echo  "My name is $my_name" . '<br />' ;
+echo  "My name is $My_Nick" . '<br />' ;
+echo  "I am $age_7 years old" . '<br />' ;
 
 ?>
