ccfc3543bec47738274f220ec8741ea19b11cb62
al-jabir <aljabir098@gmail.com>
2023-02-09 20:59:31 +0600

    variable name rules

--- practiceday.php
+++ practiceday.php
@@ -6,4 +6,15 @@ echo "<h3>My name is $name</h3>";
 echo "<br/>";
 
 echo 'Hey are you ' .$name;
+
+// variable name rules
+/*
+$name = 'King';
+
+$my_name = 'John';
+
+$My_Nick = 'Mafia';
+$age_7 = '7';
+*/
+
 ?>
