da74fb4e4ec8e05963d5419fd88111f3a1cecce3
al-jabir <aljabir098@gmail.com>
2023-02-11 01:56:45 +0600

    day logic

--- tt.php
+++ tt.php
@@ -1,5 +1,14 @@
 <?php
  $someString = "15";
  $birthyear = 1982;
- echo $someString + $birthyear;
+ echo $someString + $birthyear . '<br />';
+
+ $day = 'Saturday';
+
+ if($day == 'Saturday'){
+    echo "<h1>I will try learn advandced PHP of $day</h1>";
+ }else{
+    echo "<h3>I will another learn php</h3>";
+ }
+
 ?>
