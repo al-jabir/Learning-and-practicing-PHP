2625bf1773f749189b99bf709562ce778a4da23d
al-jabir <aljabir098@gmail.com>
2023-02-10 19:45:01 +0600

    switch

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+$bestColor = 'purple';
+
+switch ($bestColor) {
+    case 'gold':
+        echo 'I did not like gold colors.';
+        break;
+    case 'orange':
+        echo 'I like something orange colors.';
+        break;
+    case 'purple':
+        echo 'I am so very like purple colors.';
+        break;
+    default:
+        echo 'I can not find the best color';
+}
+?>
