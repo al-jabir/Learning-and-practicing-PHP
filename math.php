8e48388dbbff33425e4ffc42b7c8b18a1f444712
al-jabir <aljabir098@gmail.com>
2023-02-10 16:05:54 +0600

    done math

---
+++ math.php
@@ -0,0 +1,22 @@
+<?php
+// pi
+echo(pi()) . '<br />';
+// min and max values
+echo(min(3,5,6,77,44,22,2)) .'<br />';
+echo(max(3,5,6,77,44,22,-3,2)) .'<br />';
+
+// sqrt values
+
+echo(sqrt(81)) . '<br/>' ; // 9
+
+// round values
+
+echo(round(3.50)) . '<br/>' ;
+echo(round(1.49)). '<br/>' ;
+echo(round(1.59)). '<br/>' ;
+
+// random values
+
+echo(rand(1,9));
+
+?>
