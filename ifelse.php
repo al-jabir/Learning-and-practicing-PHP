1ca204f2afe8ccef46416b02dcd48103c86a8cb2
al-jabir <aljabir098@gmail.com>
2023-02-10 19:37:24 +0600

    ifelse logic

---
+++ ifelse.php
@@ -0,0 +1,14 @@
+<?php
+$t = date('d');
+
+if($t == '10'){
+    echo '<h1>I will be learn code of php</h1>' . '<br/>';
+}
+elseif($t != '10'){
+    echo '<h2>I will be try again good programmer.</h2>' . '<br/>';
+}
+else{
+    echo '<h3 align = "center" >I will try again good practice.</h3>';
+}
+
+?>
