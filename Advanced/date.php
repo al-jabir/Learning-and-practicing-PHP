0065eee298122524612e1729292ef3d6150d89d1
al-jabir <aljabir098@gmail.com>
2023-02-11 22:07:55 +0600

    done

--- Advanced/date.php
+++ Advanced/date.php
@@ -5,4 +5,7 @@ echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l") . "<br>";
 date_default_timezone_set("Asia/Dhaka");
 echo "The time is " . date("H:i:sa") . "<br>";
+
+date_default_timezone_set('Asia/Dhaka');
+echo "The time is " . date("h:i:sa") . "<br>";
 ?>
