7d28fcfeedc3751c08ac961090523e18d2daac4c
al-jabir <aljabir098@gmail.com>
2023-05-26 00:33:26 +0600

    test func

--- TestCode/index.php
+++ TestCode/index.php
@@ -2,4 +2,8 @@
 $userName = "I will be Developers";
 echo var_dump(@$userName);
 // echo 'When you will be good developer.';
+function test($a,$b){
+return $a * $b;
+}
+echo "The output of ". test(8, 9);
 ?>
