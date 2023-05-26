9d3dcdf2c749159da60f11c0f21c65440dc6f22f
al-jabir <aljabir098@gmail.com>
2023-05-26 10:50:59 +0600

    done array

--- Basic/arrayt.php
+++ Basic/arrayt.php
@@ -1,5 +1,32 @@
-<?php
-$user = array("Abdullah","Mafia","Mister","Gangsters");
-echo count($user) . "<br/>";
-echo var_dump($user) . "<br/>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $books = [
+        "Front end developer",
+        "Backend developer",
+        "Cloud developer",
+        "DevOps developer",
+        "AI developer",
+        "Mern Stack developer",
+        "Full stack developer"
+    ];
+    ?>
+    <ul type="square">
+        <?php
+        foreach($books as $book){
+            echo "<h1>$book</h1>";
+        }
+        ?>
+    </ul>
+</body>
+
+</html>
